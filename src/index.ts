e6a07bbb723db8fef31bffbf61028d6c637d9c43
Ethan-Prieur1 <ethan.prieur@mths.ca>
2022-09-19 14:03:06 +0000

    Added File

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+/**
+ * The program shows how to use if statements.
+ *
+ * By:      Ethan Prieur
+ * Version: 1.0
+ * Since:   2022-09-15
+ */
+
+import promptSync from 'prompt-sync'
+
+
+const prompt = promptSync()
+
+const item = prompt('What food do you want? (Pizza, Sub, Soup): ')
+const amountInt = prompt('What Quanity do you want? (1-3): ')
+let amountInt = parseInt(amountString)
+if item = 'sub'
+	time == 45
+else if
+const answer = time * percent
+console.log(`Cook time is ${ageNumber}`)
+
+
+console.log('\nDone.')
