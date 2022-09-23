a8ae27d0c323eb4afddb50ac2d6fedd2067ed692
Ethan-Prieur1 <ethan.prieur@mths.ca>
2022-09-23 13:18:33 +0000

    Third Commit

--- src/index.ts
+++ src/index.ts
@@ -8,21 +8,34 @@
 
 import promptSync from 'prompt-sync'
 
-
 const prompt = promptSync()
 
 const itemType = prompt('What Food? (Sub, Pizza, Soup): ')
-const itemAmount = prompt('How Many? (1-3): ')
-if (itemType = 'Sub') {
-	time == 45
-} else if (itemType = 'Pizza') {
-	time == 60
-} else if (itemType = 'Soup') {
-	time == 90
-} else {
-	console.log("Invalid Input!)
-if (itemAmount = 2
-console.log(`Age is >  ${ageNumber}`)
+const itemAmountString = prompt('How Many? (1-3): ')
+const itemAmountInt = parseInt(itemAmountString)
+let time = 1
+let percent = 0.5
 
+if (itemType === 'Sub') {
+  time = 45
+} else if (itemType === 'Pizza') {
+  time = 60
+} else if (itemType === 'Soup') {
+  time = 105
+} else {
+  console.log('Invalid Input!')
+}
+
+if (itemAmountInt === 2) {
+  percent = 1.5
+} else if (itemAmountInt === 3) {
+  percent = 2
+} else if (itemAmountInt === 1) {
+  percent = 1
+} else {
+  console.log('Invalid Input!')
+}
+const answer = time * percent
+console.log(`Total cook time is: ${answer} seconds`)
 
 console.log('\nDone.')
